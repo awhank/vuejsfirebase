fa7c26b09e78846e2b529b3688d64f16a5cce41a
= <awhank@gmail.com>
2020-02-21 12:07:33 +0800

    not finished

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,70 @@
 <template>
-  <div id="app">
+  <div id="app" class="container">
+    <div class="page-header">
+      <h1>Vue js 2  FIrebase Sample Application</h1>
+    </div>
+
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h3>Books Lists</h3>
+      </div>
+      <div class="panel-body">
+        <table class="table table-striped">
+          <thead>
+            <tr>
+              <th>
+                Title
+              </th>
+              <th>
+                Author
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="book in books">
+              <td>
+               <a v-bind:href="book.url">{{book.title}}</a> 
+              </td>
+              <td>
+                {{book.author}}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
     <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
 
 <script>
+// import Firebase from 'firebase'
+import firebase from 'firebase/app'
+import 'firebase/database'
+
+let config ={
+  apiKey: "AIzaSyDveQyfTwxYgpg_hBXH9VtfjHOZ-Dv6uEo",
+  authDomain: "vuejs-and-firebase-adb5c.firebaseapp.com",
+  databaseURL: "https://vuejs-and-firebase-adb5c.firebaseio.com",
+  projectId: "vuejs-and-firebase-adb5c",
+  storageBucket: "vuejs-and-firebase-adb5c.appspot.com",
+  messagingSenderId: "84882270501",
+  appId: "1:84882270501:web:5833f22cdab2a6137852cc",
+  measurementId: "G-B70TTHW4ND"
+};
+
+let app = Firebase.initializeApp(config);
+let db = app.database();
+console.log(db);
+
+let booksRef = db.ref('books');
+
 export default {
-  name: 'App'
+  name: 'App',
+  firebase: {
+    books: booksRef
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import {rtdbPlugin} from 'vuefire'
 import App from './App'
 import router from './router'
+
+Vue.use(rtdbPlugin)
 
 Vue.config.productionTip = false
 
